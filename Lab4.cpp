df09f8741724a1c0a3229b1c2d2a39d403e1abeb
sjense29 <sjense29@uwyo.edu>
2025-02-18 19:01:21 -0700

    fuck it. porgess

--- Lab4.cpp
+++ Lab4.cpp
@@ -90,9 +90,15 @@ string stringReverse(string toReverse)
 {
   stack<char> reverse; //declare the stack
   string newString = "";
-  /*
-    Your code goes here
-  */
+	for (int i = 0; i< toReverse.length(); i++) 
+	{
+		reverse.push(toReverse[i]);
+	}
+  	while (!reverse.empty()) 
+	{
+		newString = newString + reverse.top();
+     		reverse.pop();
+	}
   return newString;
 }
 
@@ -100,8 +106,12 @@ string stringReverse(string toReverse)
 bool parenCheck(string toCheck)
 {
   queue<char> checker; //declare my queue
-  /*
-    Your code goes here
-  */
+for (int i= 0; i< toCheck.length(); i++)
+	{
+	if ( toCheck   == '(')
+	{
+	
+	}
+	}
   return false; //need a return statement to compile
 }
